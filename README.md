3a457e9f1ad0d646d0d24ee007621df7c2e6b4dd
GrzegorzNecka <strzypior@gmail.com>
2020-12-29 11:33:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wc-custom-elem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/wc-custom-elem)
---
+++ index.js
@@ -0,0 +1,26 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+class Greeting extends HTMLElement {
+  static get observedAttributes() {
+    return ["name"];
+  }
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    console.log("dodano");
+  }
+  disconnectedCallback() {
+    console.log("usunięto");
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log("Custom element attributes changed.");
+  }
+}
+
+window.customElements.define("my-greeting", Greeting);
